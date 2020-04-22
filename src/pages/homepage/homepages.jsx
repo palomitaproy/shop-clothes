d65086ace74c0ca52755bcf8d9793034a8e00ae7
Paloma <palomita.c1@gmail.com>
2020-04-22 21:00:50 +0200

    add styling menu-item

---
+++ src/pages/homepage/homepages.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Directory from '../../components/directory/directory'
+
+import './homepages.scss'
+
+
+ const HomePage = () =>(
+    <div className='homepage'>
+       <Directory />
+    </div>
+
+);
+export default HomePage;
